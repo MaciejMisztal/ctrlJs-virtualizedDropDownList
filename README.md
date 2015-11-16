846464efc8b8b1c48fb8ae2f38e488a0d312ff3f
mim <mim@nasz>
2015-11-16 05:32:57 +0100

    added info about Knockout integrating and fix for Bootstrap3 modal

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Sample JS: virtualized dropdownlist powered by local data (using Kendo UI (core)).
 Install deps:
   bower install
+Tip 1: integrating with KnockoutJs: http://volaresystems.com/blog/post/2013/02/18/Integrating-Kendo-UI-drop-downs-with-Knockout-JS
+Tip 2: integrating with Bootstrap 3 modal: file bt3ModaKendoUiCorelFix.js
---
+++ bt3ModaKendoUiCorelFix.js
@@ -0,0 +1,28 @@
+﻿/*
+* Fix for interaction Kendo UI Core with Bootstrap3 modal.
+* Source: http://www.telerik.com/forums/dropdownlist-closes-when-setting-filter-option
+*/
+(function ($, kendo) {
+
+    var  _init = kendo.ui.Popup.fn.init;
+
+    var Popup = kendo.ui.Popup.extend({
+        init: function (element, options) {
+            // Only set appendTo if nothing was manually set in the options.
+            if (options.appendTo === undefined) {
+                // Find the nearest parent bootstrap modal, if any.
+                var parentModal = $(options.anchor).closest('.modal');
+
+                // Found one!
+                if (parentModal.length > 0) {
+                    options.appendTo = parentModal[0];
+                }
+            }
+
+            // Call the base constructor.
+            _init.call(this, element, options);
+        }
+    });
+
+    kendo.ui.plugin(Popup);
+}(window.kendo.jQuery, window.kendo));
